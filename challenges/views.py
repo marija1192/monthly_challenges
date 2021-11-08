9f4c1c2c17a3772b21183d6cb2303f99e6ba5c1a
marija1192 <marija.jankovic.1192@gmail.com>
2021-11-08 22:11:02 +0100

    extend templates

--- challenges/views.py
+++ challenges/views.py
@@ -14,22 +14,18 @@ monthly_challenges = {
     "september": "Learn Django for 20 minutes!",
     "october": "Learn Django for 20 minutes!",
     "november": "Learn Django for 20 minutes!",
-    "december": "Learn Django for 20 minutes!"
+    "december": None
 }
 
 # Create your views here.
 
 def index(request):
-    list_items = ""
+    # list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
@@ -44,7 +40,10 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request ,"challenges/challenge.html")
+        return render(request ,"challenges/challenge.html", {
+            "text": challenge_text,
+            "month": month,
+        })
     except:
         return HttpResponseNotFound("<h1>Not supported</h1>")
         
