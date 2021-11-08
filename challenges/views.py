0a51c843740dc068ac1e09d444e902497d2a55ff
marija1192 <marija.jankovic.1192@gmail.com>
2021-11-08 16:06:37 +0100

    about templates

--- challenges/views.py
+++ challenges/views.py
@@ -44,8 +44,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data)
+        return render(request ,"challenges/challenge.html")
     except:
         return HttpResponseNotFound("<h1>Not supported</h1>")
         
